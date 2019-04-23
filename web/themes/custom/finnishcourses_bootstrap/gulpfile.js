cbfb650c0090d3376c6ed235a38e0a2a7f12086d
Sarah Robertz <robertz.sarah@gmail.com>
2019-04-23 12:55:22 +0300

    Minor fixes with configuration

--- web/themes/custom/finnishcourses_bootstrap/gulpfile.js
+++ web/themes/custom/finnishcourses_bootstrap/gulpfile.js
@@ -54,10 +54,10 @@ function js () {
     .pipe(browserSync.stream())
 }
 
-// Static Server + watching scss/html files (OBS! LANDO URL)
+// Static Server + watching scss/html files OBS! LANDO URL
 function serve () {
   browserSync.init({
-    proxy: 'http://fc.lndo.site/web',
+    proxy: 'http://fc.lndo.site/web/',
   })
 
   gulp.watch([paths.scss.watch, paths.scss.bootstrap], styles).on('change', browserSync.reload)
