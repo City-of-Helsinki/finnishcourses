58e1bada4dc1862d83a47d271d218efe60929534
Sarah Robertz <sarah.robertz@digia.com>
2019-08-29 10:16:28 +0300

    Targeting "View search course" button if enters or exits viewport
    Also, showing "View search course" sticky mobile button, when "View search course" button have exited the viewport.

--- web/themes/custom/finnishcourses_bootstrap/js/global.js
+++ web/themes/custom/finnishcourses_bootstrap/js/global.js
@@ -25,6 +25,26 @@
         $(".more-search-options-trigger").text($(".more-search-options-trigger").text()
         === lessSearch ? moreSearch : lessSearch);
       });
+
+      // STICKY MOBILE BUTTON
+      // Observer API
+      // Root is the browser viewport / screen
+      let observer = new IntersectionObserver(function(entries) {
+        // Since there is a single target to be observed, there will be only one entry
+          if(entries[0]['isIntersecting'] === true) {
+           if(entries[0]['intersectionRatio'] > 0.5)
+              // More or less than 50% of target is showing on the screen
+            $("#edit-submit-search-courses").hide();
+          }
+          else {
+            // Target is not visible on the screen, show sticky mobile button
+            if ($(window).width() < 844.98) {
+              $("#edit-submit-search-courses").show();
+            }
+          }
+        }, { threshold: [0, 0.5, 1] });
+
+      observer.observe(document.querySelector("#edit-submit-fixed"));
     }
   };
 })(jQuery, Drupal);
