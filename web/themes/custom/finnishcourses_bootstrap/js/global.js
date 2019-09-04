4bceefec010a9c492553f7d36ae84106fafb7e10
Sarah Robertz <sarah.robertz@digia.com>
2019-09-04 07:57:43 +0300

    Added opacity to mobile menu

--- web/themes/custom/finnishcourses_bootstrap/js/global.js
+++ web/themes/custom/finnishcourses_bootstrap/js/global.js
@@ -63,27 +63,26 @@
       }
 
       // HAMBURGER MENU
-      $( ".mobile-menu-icon" ).click(function() {
-        if ($(window).width() < 844) {
+        $(".mobile-menu-icon").click(function () {
           $(".mobile-menu-wrapper").slideToggle("fast", function () {
-            $(".mobile-menu-icon").hide();
-            $(".fixed-top").css('backgroundColor', '#0073cf'); // Fix this
-            $( ".mobile-close-icon" ).show();
+            if ($(window).width() < 844) {
+              $(".mobile-menu-icon").hide();
+              $(".fixed-top").css('backgroundColor', '#0073cf'); // Fix this
+              $(".fixed-top").css({opacity: 0.96}); // Fix this
+              $(".mobile-close-icon").show();
+            }
           });
-        }
-      });
+        });
 
-      $( ".mobile-close-icon" ).click(function() {
-        if ($(window).width() < 844) {
+        $(".mobile-close-icon").click(function () {
           $(".mobile-menu-wrapper").slideToggle(function () {
-            $(".mobile-close-icon").hide();
-            $( ".mobile-menu-icon" ).show();
-            $(".fixed-top").css('backgroundColor', 'transparent'); // Fix this
+            if ($(window).width() < 844) {
+              $(".mobile-close-icon").hide();
+              $(".mobile-menu-icon").show();
+              $(".fixed-top").css('backgroundColor', 'transparent'); // Fix this
+            }
           });
-        }
-      });
-
-
+        });
     }
   };
 })(jQuery, Drupal);
