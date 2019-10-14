0ed927801b9d4e12e87ae97c1fdf06190c1d1a36
Teemu Rautiainen <teemu.rautiainen@verkkojulkaisut.fi>
2019-10-14 15:46:09 +0300

    Implemented Go back link and themed it

---
+++ web/modules/custom/finnishcourses_base/src/Plugin/Block/GoBackLink.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\finnishcourses_base\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\node\NodeInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\StringTranslation\TranslationInterface;
+use Drupal\Core\Cache\Cache;
+
+/**
+ * Provides a 'Go back link' block.
+ *
+ * @Block(
+ *  id = "go_back_link",
+ *  admin_label = @Translation("Go back link"),
+ * )
+ */
+class GoBackLink extends BlockBase {
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $goBackLink = t('Go back');
+
+    return [
+      '#children' => '<a class="go-back-link" href="#" onclick="history.go(-1)">'.$goBackLink.'</a>',
+    ];
+  }
+
+}
